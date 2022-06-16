12c4e8f930d4cfe468c50ef5078f6d6da8c58971
Uriel Guillermo Daniel <45216219+Uriel1989@users.noreply.github.com>
2022-06-15 21:49:10 -0300

    Add files via upload

---
+++ 81-IngresarNombreEdad/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+/*Pedir por consola tu nombre y tu edad y mostrar el siguiente mensaje: 
+ * “Te llamas ” <nombre> ” y tienes ” <años> ” años”
+ */
+
+
+
+namespace _81_IngresarNombreEdad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese el nombre y la edad:");
+
+            string n = Convert.ToString(Console.ReadLine());
+
+            int e = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Te llamas " + n + " " + "y tienes " + e + " años");
+        }
+    }
+}
